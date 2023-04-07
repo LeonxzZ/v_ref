63d6aa0f91531665b4596ad86c786629499a9f77
xzz <1102817050@qq.com>
2023-04-07 17:01:00 +0800

    第一次提交

---
+++ src/components/SchoolInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="school">
+    <h2>1: {{ name }}</h2>
+    <h2>2: {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SchoolInfo",
+  data() {
+    return {
+      name: "scName",
+      address: "scAdd",
+    };
+  },
+};
+</script>
+
+<style>
+.school {
+  background-color: beige;
+}
+</style>
